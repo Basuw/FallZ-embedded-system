68b62b5e580f993ee8e858df67f6fd7c51237a1e
bajacqueli <bastien.jacquelin@etu.uca.fr>
2025-01-14 14:27:05 +0100

    refacto main

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,6 @@
 
 // Création d'une instance du capteur
 Adafruit_MPU6050 mpu;
-ArduinoLEDMatrix matrix;
-
-// Seuils de détection
-#define G_THRESHOLD 12.0             // Accélération en m/s² (≈ 1.5g)
-#define ROTATION_THRESHOLD 0.08      // Rotation en rad/s
-#define FALL_DURATION_THRESHOLD 0.2  // Durée minimale d'une chute en secondes
 
 // Fréquence d'échantillonnage
 #define SAMPLING_RATE 0.01          // 100 Hz
@@ -19,22 +13,9 @@ ArduinoLEDMatrix matrix;
 bool fall_detected = false;
 unsigned long fall_start_time = 0;
 
-const uint32_t happy[] = {
-    0x19819,
-    0x80000001,
-    0x81f8000
-};
-const uint32_t heart[] = {
-    0x3184a444,
-    0x44042081,
-    0x100a0040
-};
-
 void setup() {
   Serial.begin(115200);
   Serial.println("Initialisation du MPU6050...");
-  matrix.begin();
-  matrix.loadFrame(happy);
 
   // Initialiser le MPU6050
   if (!mpu.begin(0x69)) {
@@ -50,31 +31,19 @@ void setup() {
 }
 
 void loop() {
-  // Lire les données du capteur
-  matrix.loadFrame(happy);
-  sensors_event_t accel, gyro, temp;
-  mpu.getEvent(&accel, &gyro, &temp);
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
-  // Extraire les valeurs d'accélération (m/s²) et de rotation (rad/s)
-  float ax = accel.acceleration.x;
-  float ay = accel.acceleration.y;
-  float az = accel.acceleration.z;
+  // m/s^2
+  Serial.print("acc_x :"); Serial.print(a.acceleration.x);
+  Serial.print(", acc_y :"); Serial.print(a.acceleration.y);
+  Serial.print(", acc_z :"); Serial.print(a.acceleration.z);
+  // rad/s
+  Serial.print(", gy_x :"); Serial.print(g.gyro.x);
+  Serial.print(", gy_y :"); Serial.print(g.gyro.y);
+  Serial.print(", gy_z :"); Serial.println(g.gyro.z);
 
-  float gx = radians(gyro.gyro.x);  // Conversion en rad/s
-  float gy = radians(gyro.gyro.y);
-  float gz = radians(gyro.gyro.z);
 
-  unsigned long currentTime = millis();
-  unsigned long milliseconds = currentTime % 1000;
-
-  // Envoyer les données via le port série (format CSV)
-  // Serial.print("t: ");Serial.print(milliseconds);Serial.print(", ");
-  Serial.print("acc_x :"); Serial.print(ax);
-  Serial.print(", acc_y :"); Serial.print(ay);
-  Serial.print(", acc_z :"); Serial.print(az);
-  Serial.print(", gy_x :"); Serial.print(gx);
-  Serial.print(", gy_y :"); Serial.print(gy);
-  Serial.print(", gy_z :"); Serial.println(gz);
-
-  delay(21);
+  delay(SAMPLING_RATE);
 }
